afecb712cfee0e43c727a23231a0b488a1911a23
Ryan Dhungel <kaloraat@gmail.com>
2019-01-26 10:55:52 +1100

    24 show edit delete buttons

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { isAuthenticated } from "../auth";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
+import { read } from "./apiUser";
 
 class Profile extends Component {
     constructor() {
@@ -11,40 +12,55 @@ class Profile extends Component {
         };
     }
 
+    init = userId => {
+        const token = isAuthenticated().token;
+        read(userId, token).then(data => {
+            if (data.error) {
+                this.setState({ redirectToSignin: true });
+            } else {
+                this.setState({ user: data });
+            }
+        });
+    };
+
     componentDidMount() {
         const userId = this.props.match.params.userId;
-        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${isAuthenticated().token}`
-            }
-        })
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                if (data.error) {
-                    this.setState({ redirectToSignin: true });
-                } else {
-                    this.setState({ user: data });
-                }
-            });
+        this.init(userId);
     }
 
     render() {
-        const redirectToSignin = this.state.redirectToSignin;
+        const { redirectToSignin, user } = this.state;
         if (redirectToSignin) return <Redirect to="/signin" />;
 
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Profile</h2>
-                <p>Hello {isAuthenticated().user.name}</p>
-                <p>Email: {isAuthenticated().user.email}</p>
-                <p>{`Joined ${new Date(
-                    this.state.user.created
-                ).toDateString()}`}</p>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2 className="mt-5 mb-5">Profile</h2>
+                        <p>Hello {isAuthenticated().user.name}</p>
+                        <p>Email: {isAuthenticated().user.email}</p>
+                        <p>{`Joined ${new Date(
+                            user.created
+                        ).toDateString()}`}</p>
+                    </div>
+
+                    <div className="col-md-6">
+                        {isAuthenticated().user &&
+                            isAuthenticated().user._id == user._id && (
+                                <div className="d-inline-block mt-5">
+                                    <Link
+                                        className="btn btn-raised btn-success mr-5"
+                                        to={`/user/edit/${user._id}`}
+                                    >
+                                        Edit Profile
+                                    </Link>
+                                    <button className="btn btn-raised btn-danger">
+                                        Delete Profile
+                                    </button>
+                                </div>
+                            )}
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/user/apiUser.js
@@ -0,0 +1,14 @@
+export const read = (userId, token) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
