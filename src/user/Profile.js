9f90870aa79afef4411591c8bc0d144b50792eb7
Ryan Dhungel <kaloraat@gmail.com>
2019-01-31 00:15:27 +1100

    57 display following list

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -5,6 +5,7 @@ import { read } from "./apiUser";
 import DefaultProfile from "../images/avatar.jpg";
 import DeleteUser from "./DeleteUser";
 import FollowProfileButton from "./FollowProfileButton";
+import ProfileTabs from "./ProfileTabs";
 
 class Profile extends Component {
     constructor() {
@@ -119,6 +120,11 @@ class Profile extends Component {
                         <hr />
                         <p className="lead">{user.about}</p>
                         <hr />
+
+                        <ProfileTabs
+                            followers={user.followers}
+                            following={user.following}
+                        />
                     </div>
                 </div>
             </div>
---
+++ src/user/ProfileTabs.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import DefaultProfile from "../images/avatar.jpg";
+
+class ProfileTabs extends Component {
+    render() {
+        const { following, followers } = this.props;
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <h3 className="text-primary">Followers</h3>
+                        <hr />
+                        {followers.map((person, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/user/${person._id}`}>
+                                        <img
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfile}`)
+                                            }
+                                            src={`${
+                                                process.env.REACT_APP_API_URL
+                                            }/user/photo/${person._id}`}
+                                            alt={person.name}
+                                        />
+                                        <div>
+                                            <p className="lead">
+                                                {person.name}
+                                            </p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="col-md-4">
+                        <h3 className="text-primary">Following</h3>
+                        <hr />
+                        {following.map((person, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/user/${person._id}`}>
+                                        <img
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfile}`)
+                                            }
+                                            src={`${
+                                                process.env.REACT_APP_API_URL
+                                            }/user/photo/${person._id}`}
+                                            alt={person.name}
+                                        />
+                                        <div>
+                                            <p className="lead">
+                                                {person.name}
+                                            </p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="col-md-4">
+                        <h3 className="text-primary">Posts</h3>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProfileTabs;
