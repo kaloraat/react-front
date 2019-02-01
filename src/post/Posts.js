2d25b53deca556288e9ea1f98df302615a67ff8b
Ryan Dhungel <kaloraat@gmail.com>
2019-02-02 01:24:55 +1100

    show posts with image

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { list } from "./apiPost";
-// import DefaultProfile from "../images/avatar.jpg";
+import DefaultPost from "../images/mountains.jpg";
 import { Link } from "react-router-dom";
 
 class Posts extends Component {
@@ -21,24 +21,56 @@ class Posts extends Component {
         });
     }
 
-    renderPosts = posts => (
-        <div className="row">
-            {posts.map((post, i) => (
-                <div className="card col-md-4" key={i}>
-                    <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <p className="card-text">{post.body}</p>
-                        <Link
-                            to={`/posts/${post._id}`}
-                            className="btn btn-raised btn-primary btn-sm"
-                        >
-                            Read more
-                        </Link>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+    renderPosts = posts => {
+        return (
+            <div className="row">
+                {posts.map((post, i) => {
+                    const posterId = post.postedBy
+                        ? `/user/${post.postedBy._id}`
+                        : "";
+                    const posterName = post.postedBy
+                        ? post.postedBy.name
+                        : " Unknown";
+
+                    return (
+                        <div className="card col-md-4" key={i}>
+                            <div className="card-body">
+                                <img
+                                    src={`${
+                                        process.env.REACT_APP_API_URL
+                                    }/post/photo/${post._id}`}
+                                    alt={post.title}
+                                    onError={i =>
+                                        (i.target.src = `${DefaultPost}`)
+                                    }
+                                    className="img-thunbnail mb-3"
+                                    style={{ height: "200px", width: "auto" }}
+                                />
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">
+                                    {post.body.substring(0, 100)}
+                                </p>
+                                <br />
+                                <p className="font-italic mark">
+                                    Posted by{" "}
+                                    <Link to={`${posterId}`}>
+                                        {posterName}{" "}
+                                    </Link>
+                                    on {new Date(post.created).toDateString()}
+                                </p>
+                                <Link
+                                    to={`/posts/${post._id}`}
+                                    className="btn btn-raised btn-primary btn-sm"
+                                >
+                                    Read more
+                                </Link>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
 
     render() {
         const { posts } = this.state;
