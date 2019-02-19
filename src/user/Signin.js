d450e208e5f0ce58babf0f9d55dd386df965c9c0
Ryan Dhungel <kaloraat@gmail.com>
2019-02-19 18:39:54 +1100

    recaptcha

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
-import Recaptcha from "react-recaptcha";
+import { Recaptcha } from "react-recaptcha";
 import { signin, authenticate } from "../auth";
 import SocialLogin from "./SocialLogin";
 
